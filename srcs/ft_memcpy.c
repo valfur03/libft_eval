64644eed7acd5bc613de0a4ee421a5e24747ea53
Valentin <valentin.furmanek@gmail.com>
2020-09-23 12:36:48 +0200

    Add more tests to ft_memcpy

--- srcs/ft_memcpy.c
+++ srcs/ft_memcpy.c
@@ -6,27 +6,81 @@
 /*   By: vbotte <vbotte@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/08 19:10:10 by vbotte            #+#    #+#             */
-/*   Updated: 2020/09/19 14:16:03 by vbotte           ###   ########.fr       */
+/*   Updated: 2020/09/23 12:35:10 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft_eval.h"
 
-struct {
-    char    name[40];
-    int     age;
-}           person, person_copy;
-
-int main()
+void	test1(void)
 {
-    char myname[] = "Pierre de Fermat";
-    ft_memcpy(person.name, myname, strlen(myname) + 1);
-    
-    person.age = 46;
-    ft_memcpy(&person_copy, &person, sizeof(person));
+	char	str[] = "Hello world and 42";
 
-    printf("User:\nperson_copy: %s, %d \n", person_copy.name, person_copy.age);
-    printf("Expected:\nperson_copy: Pierre de Fermat, 46\n");
+	{
+		char	buffer[6];
 
-    return 0;
+		ft_memcpy(buffer, str, 6);
+		buffer[5] = '\0';
+		printf("User		: %s\n", buffer);
+	}
+	{
+		char	buffer[6];
+
+		memcpy(buffer, str, 6);
+		buffer[5] = '\0';
+		printf("Expected	: %s\n", buffer);
+	}
+}
+
+void	test2(void)
+{
+	{
+		char	str[] = "Hello world and 42";
+
+		ft_memcpy(str + 2, str, 6);
+		printf("User		: %s\n", str);
+	}
+	{
+		char	str[] = "Hello world and 42";
+
+		memcpy(str + 2, str, 6);
+		printf("Expected	: %s\n", str);
+	}
+}
+
+void	test3(void)
+{
+	int	arr[] = { 1, 23, 54, 0, 74, 234, 13689 };
+
+	{
+		int	buffer[7];
+
+		memset(buffer, '\0', 7 * sizeof(*buffer));
+		ft_memcpy(buffer, arr, 7 * sizeof(*buffer));
+		printf("User		: ");
+		for (int i = 0; i < 7; i++)
+    	    printf("%d ", buffer[i]);
+		printf("\n");
+	}
+	{
+		int	buffer[7];
+
+		memset(buffer, '\0', 7 * sizeof(*buffer));
+		memcpy(buffer, arr, 7 * sizeof(*buffer));
+		printf("Expected	: ");
+		for (int i = 0; i < 7; i++)
+    	    printf("%d ", buffer[i]);
+		printf("\n");
+	}
+}
+
+int		main(void)
+{
+	printf("===== TEST1 =====\n\n");
+	test1();
+	printf("===== TEST2 =====\n\n");
+	test2();
+	printf("===== TEST3 =====\n\n");
+	test3();
+	return (0);
 }
